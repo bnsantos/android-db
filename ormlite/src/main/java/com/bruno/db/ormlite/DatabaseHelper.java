ab237265642a1192f9d5d9ab934a6413723ec5cd
bnsantos <brunonascimentosantos@gmail.com>
2015-03-31 14:37:32 -0300

    DB helper

---
+++ ormlite/src/main/java/com/bruno/db/ormlite/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.bruno.db.ormlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Created by bruno on 31/03/15.
+ */
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+    private static final String DATABASE_NAME = "com.bruno.db.ormlite";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+
+    }
+}
